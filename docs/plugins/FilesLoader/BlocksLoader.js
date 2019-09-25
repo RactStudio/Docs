13b7db7def1e7ef87f924164588825ab101dd312
Mahmudul Hasan Rubel <bd.mhrubel@gmail.com>
2019-09-26 05:34:53 +0600

    Update BlocksLoader.js

--- docs/plugins/FilesLoader/BlocksLoader.js
+++ docs/plugins/FilesLoader/BlocksLoader.js
@@ -1,35 +1,27 @@
-window.onload = function() {
-	var elements = document.getElementsByTagName('*'),
-		i;
-	for (i in elements) {
-		if (elements[i].hasAttribute && elements[i].hasAttribute('data-blocks')) {
-			fragment(elements[i], elements[i].getAttribute('data-blocks'));
-		}
-	}
-	function fragment(el, url) {
-		var localTest = /^(?:file):/,
-			xmlhttp = new XMLHttpRequest(),
-			status = 0;
-
-		xmlhttp.onreadystatechange = function() {
-			/* if we are on a local protocol, and we have response text, we'll assume
- *  				things were sucessful */
-			if (xmlhttp.readyState == 4) {
-				status = xmlhttp.status;
-			}
-			if (localTest.test(location.href) && xmlhttp.responseText) {
-				status = 200;
-			}
-			if (xmlhttp.readyState == 4 && status == 200) {
-				el.outerHTML = xmlhttp.responseText;
-			}
-		}
-
-		try { 
-			xmlhttp.open("GET", url, true);
-			xmlhttp.send();
-		} catch(err) {
-			/* todo catch error */
-		}
-	}
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("rs-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("rs-include-html");
+          includeHTML();
+        }
+      }
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
 }
